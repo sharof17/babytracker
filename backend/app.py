d060732a10cfb578d40d6a7a225a67c76dd563c5
Sharof <abd.sharof@gmail.com>
2022-10-25 23:24:51 -0500

    Adding Final draft

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
@@ -18,9 +18,58 @@ class Event(db.Model):
     def __init__(self, description):
         self.description = description
 
+def format_event(event):
+    return {
+        "description": event.description,
+        "id": event.id,
+        "created_at": event.created_at
+    }
+
 @app.route('/')
 def hello():
     return 'Hey!'
+
+# create an event
+@app.route('/events', methods = ['POST'])
+def create_event():
+    description = request.json['description']
+    event = Event(description)
+    db.session.add(event)
+    db.session.commit()
+    return format_event(event)
+
+#get all events
+@app.route('/events', methods = ['GET'])
+def get_events():
+    events = Event.query.order_by(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return {'events': event_list}
+
+# get single event
+@app.route('/events/<id>')
+def get_event(id):
+    event = Event.query.filter_by(id=id).one()
+    formatted_event = format_event(event)
+    return {'event': formatted_event}
+
+# delete an event
+@app.route('/events/<id>', methods = ["DELETE"])
+def delete_event(id):
+    event = Event.query.filter_by(id=id).one()
+    db.session.delete(event)
+    db.session.commit()
+    return f"Event (id = {id}) was deleted!"
+
+# edit an event
+@app.route('/events/<id>', methods = ['PUT'])
+def update_event(id):
+    event = Event.query.filter_by(id=id)
+    description = request.json['description']
+    event.update(dict(description = description))
+    db.session.commit()
+    return {'event': format_event(event.one())}
 
 if __name__ == '__main__':
     app.run()
